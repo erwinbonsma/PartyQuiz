92d172c350550794f3e62ee7981c1d4793500615
Erwin Bonsma <eriban@users.sourceforge.net>
2024-07-20 16:10:44 +0200

    Simple registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  return (
+	const [nameInput, setNameInput] = useState('');
+	const [errorMessage, setErrorMessage] = useState();
+
+	const handleInputChange = (event) => {
+		setNameInput(event.target.value);
+		setErrorMessage('');
+	};
+
+	const handleRegistration = (event) => {
+		event.preventDefault();
+
+		console.info(`Handle registration of ${nameInput}`);
+	}
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+            <form onSubmit={handleRegistration} >
+            <Row><Col><h4>Registration</h4></Col></Row>
+            <Row><Col xs={12} sm={4}><p>Name:</p></Col>
+                 <Col xs={12} sm={8}><input size={20} type="text" value={nameInput} onChange={handleInputChange} /></Col></Row>
+            <Row className="justify-content-md-center">
+                <Col xs={6} sm={4}><Button type="submit" disabled={nameInput === '' || nameInput.length > 12} >Join Quiz</Button></Col></Row>
+            </form>
+            { errorMessage &&
+                <Row><Col><p className="Error">{errorMessage}</p></Col></Row>
+            }
+        </Container>
     </div>
   );
 }
