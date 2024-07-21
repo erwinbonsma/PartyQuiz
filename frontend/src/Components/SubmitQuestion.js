3c88834491fad9f37bd0cf3f58c191ba7ec75ea4
Erwin Bonsma <eriban@users.sourceforge.net>
2024-07-21 21:20:28 +0200

    Create form for SubmitQuestion component

--- frontend/src/Components/SubmitQuestion.js
+++ frontend/src/Components/SubmitQuestion.js
@@ -1,9 +1,48 @@
 import { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+import config from '../utils/config';
 
 export function SubmitQuestion({ websocket, quizId, onDone }) {
+    const choices = Array(config.NUM_CHOICES).fill(0).map((_, idx) => idx + 1);
+
     return <>
-        <Button onClick={onDone}>Submit Question</Button>
+        <h1>Your Question</h1>
+        <Form>
+            <Form.Group as={Row} controlId="formQuestion">
+                <Col sm={0} lg={2} xl={3} />
+                <Form.Label column sm={2} xl={1}>Question</Form.Label>
+                <Col sm={9} lg={6} xl={5}>
+                    <Form.Control as="textarea" rows={3} />
+                </Col>
+            </Form.Group>
+            <br/>
+            { choices.map((choice, _) => <>
+                <Form.Group as={Row} controlId={`formAnswerGroup-${choice}`}>
+                    <Col sm={0} lg={2} xl={3} />
+                    <Form.Label column sm={2} xl={1}>Answer {choice}</Form.Label>
+                    <Col sm={9} lg={6} xl={5}>
+                        <Form.Control type="input" controlId={`formAnswer-${choice}`} />
+                    </Col>
+                </Form.Group>
+                <br/>
+            </>)}
+            <Form.Group as={Row}>
+                <Col sm={0} lg={2} xl={3} />
+                <Form.Label column sm={2} xl={1}>Solution</Form.Label>
+                <Col sm={9} lg={6} xl={5}>
+                    <Form.Select>
+                    { choices.map((choice, _) =>
+                        <option value={choice}>Answer {choice}</option>)}
+                    </Form.Select>
+                </Col>
+            </Form.Group>
+            <br/>
+            <Button onClick={onDone}>Submit Question</Button>
+        </Form>
     </>;
 }
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,7 @@
 const configSettings = {
 	SERVICE_ENDPOINT: 'ws://127.0.0.1:8765',
+
+    NUM_CHOICES: 4,
 }
 
 export default configSettings;
